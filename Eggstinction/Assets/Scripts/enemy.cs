856be3eedbdcdfe87b9673c0ff25856d4847943f
PS230050 <iwandkinderen1@gmail.com>
2025-04-14 14:03:36 +0200

    working enemy damager

--- Eggstinction/Assets/Scripts/enemy.cs
+++ Eggstinction/Assets/Scripts/enemy.cs
@@ -4,6 +4,7 @@ public class Enemy : MonoBehaviour
 {
     public float speed = 2f;
     public LayerMask wallLayer;
+    public LayerMask playerLayer;
 
     private bool movingRight = true;
     private Rigidbody2D rb;
@@ -20,10 +21,23 @@ public class Enemy : MonoBehaviour
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
+        // Check for wall collision
         if (((1 << collision.gameObject.layer) & wallLayer) != 0)
         {
             movingRight = !movingRight;
             transform.localScale = new Vector2(-transform.localScale.x, transform.localScale.y);
+        }
+
+        // Check for player collision from above
+        if (((1 << collision.gameObject.layer) & playerLayer) != 0)
+        {
+            foreach (ContactPoint2D contact in collision.contacts)
+            {
+                if (contact.normal.y < -0.5f) // Player hit from above
+                {
+                    Destroy(gameObject);
+                }
+            }
         }
     }
 }
