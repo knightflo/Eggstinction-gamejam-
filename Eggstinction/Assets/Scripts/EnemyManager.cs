10447ff74fff2da29b171ea33a1baaf371f24cc5
BasPennings <p.freekje@gmail.com>
2025-04-15 13:23:57 +0200

    #13 Resolved bugs in the enemy manager
    These bugs included not all enemies despawning and enemies still spawning after ResetEnemies is called

--- Eggstinction/Assets/Scripts/EnemyManager.cs
+++ Eggstinction/Assets/Scripts/EnemyManager.cs
@@ -14,9 +14,12 @@ public class EnemyManager : AbstractSingleton<EnemyManager>
     [SerializeField] private GameObject _enemyObjectsParent;
     [SerializeField] private SpawnPoint[] _enemySpawnPoints;
     private GameObject[] enemyObjects;
+    private Coroutine enemySpawningRoutine;
+
 
     public void ResetEnemies()
     {
+        StopEnemySpawning();
         DestroyEnemies();
         StartEnemySpawning();
     }
@@ -27,23 +30,33 @@ public class EnemyManager : AbstractSingleton<EnemyManager>
     private void DestroyEnemies()
     {
         foreach (var enemy in enemyObjects) Destroy(enemy);
+        enemyObjects = null;
     }
-    
-    public void StartEnemySpawning()
+
+    private void StopEnemySpawning()
     {
-        StartCoroutine(InstantiateEnemiesWithDelay());
+        if (enemySpawningRoutine != null)
+        {
+            StopCoroutine(enemySpawningRoutine);
+            enemySpawningRoutine = null;
+        }
+    }
+
+    private void StartEnemySpawning()
+    {
+        enemySpawningRoutine = StartCoroutine(InstantiateEnemiesWithDelay());
 
         IEnumerator InstantiateEnemiesWithDelay()
         {
             var totalEnemies = _enemySpawnPoints.Sum((sp) => sp.EnemyCount);
             enemyObjects = new GameObject[totalEnemies];
 
-            for (int i = 0; i < _enemySpawnPoints.Length; i++)
+            for (int i = 0, index = 0; i < _enemySpawnPoints.Length; i++)
             {
                 var spawnPoint = _enemySpawnPoints[i];
-                for (int j = 0; j < spawnPoint.EnemyCount; j++)
+                for (int j = 0; j < spawnPoint.EnemyCount; j++, index++)
                 {
-                    enemyObjects[i + j] = InstantiateEnemy(spawnPoint.SpawnPosition);
+                    enemyObjects[index] = InstantiateEnemy(spawnPoint.SpawnPosition);
                     yield return new WaitForSeconds(_enemySpawningDelay);
                 }
             }
