a608b75f53673a8c89c5ba7cc57d07d0c0bc7573
BasPennings <p.freekje@gmail.com>
2025-05-03 11:15:30 +0200

    UI

---
+++ Eggstinction/Assets/Scripts/UIControllerManager.cs
@@ -0,0 +1,18 @@
+using Bas.Pennings.DevTools;
+using System;
+using UnityEngine;
+
+public class UIControllerManager : AbstractSingleton<UIControllerManager>
+{
+    [SerializeField] private RectTransform _pauseMenu;
+    private RectTransform currentMenu;
+
+    //public void ToggleUI()
+    //    => currentMenu.enab
+
+    //[Serializable]
+    //public struct Menu
+    //{
+    //    public RectTransform Menu;
+    //}
+}
